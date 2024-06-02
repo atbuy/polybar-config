9a189eb89a9c52328a2917a04d5781bd359a7ce4
atbuy <filip1253@gmail.com>
2024-06-02 04:39:20 +0300

    Added polybar configuration

---
+++ launch.sh
@@ -0,0 +1,9 @@
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload toph &
+  done
+else
+  polybar --reload toph &
+fi
